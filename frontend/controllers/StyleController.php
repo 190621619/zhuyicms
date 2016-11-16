2413aece44a4be915cabaff6a8b4b368ec938c2a
peng zhao <peng.zhao@condenast.com.cn>
2016-11-16 17:08:36 +0800

    123

--- frontend/controllers/StyleController.php
+++ frontend/controllers/StyleController.php
@@ -833,7 +833,7 @@ class StyleController extends Controller {
             // 查询风格
             $styleModel = new \common\models\ZyStyle();
             //$res = $styleModel->findOne(['user_id'=>$user_id])->orderBy('create_time DESC');
-            return 1;
+            return "8,20$1,10$10,5$";
         }
     }
 
